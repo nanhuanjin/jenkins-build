3fa0acf3b1da3e21dd33d25c3dde02132a104ff0
breeze <breeze@163.com>
2020-07-01 10:18:19 +0800

    feat：测试jenkins构建流水线

---
+++ src/test/java/com/breeze/jenkins/build/JenkinsBuildApplicationTests.java
@@ -0,0 +1,13 @@
+package com.breeze.jenkins.build;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsBuildApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
